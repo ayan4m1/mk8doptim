6260caf3ef9c1834ac455593c393b3486daa3cb9
ayan4m1 <andrew@bulletlogic.com>
2025-02-06 17:58:22 -0500

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,8 @@
+import globals from 'globals';
 import eslint from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
 import { config, configs } from 'typescript-eslint';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import eslintPluginImportX from 'eslint-plugin-import-x';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
@@ -8,5 +11,25 @@ export default config(
   ...configs.recommended,
   eslintPluginImportX.flatConfigs.recommended,
   eslintPluginImportX.flatConfigs.typescript,
+  {
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin
+    },
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/jsx-uses-react': 0,
+      'react/react-in-jsx-scope': 0
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
   eslintPluginPrettier
 );
