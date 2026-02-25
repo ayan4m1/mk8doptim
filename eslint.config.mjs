c2be5921459a9a86fa6b425044dc021ec1d6205a
ayan4m1 <andrew@bulletlogic.com>
2026-02-25 10:24:59 -0500

    chore: align eslint config with others

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,25 @@
 import globals from 'globals';
 import eslint from '@eslint/js';
+import { configs } from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 import reactPlugin from 'eslint-plugin-react';
-import { config, configs } from 'typescript-eslint';
-import { configs as hooksConfigs } from 'eslint-plugin-react-hooks';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import { flatConfigs as importConfigs } from 'eslint-plugin-import-x';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
-export default config([
+export default defineConfig(
   eslint.configs.recommended,
   ...configs.recommended,
   importConfigs.recommended,
+  importConfigs.react,
   importConfigs.typescript,
-  hooksConfigs['recommended-latest'],
+  reactPlugin.configs.flat['recommended'],
+  reactHooksPlugin.configs.flat['recommended-latest'],
   {
-    plugins: {
-      react: reactPlugin
-    },
     languageOptions: {
       globals: globals.browser
     },
     rules: {
-      ...reactPlugin.configs.recommended.rules,
       'react/jsx-uses-react': 0,
       'react/jsx-sort-props': 2,
       'react/react-in-jsx-scope': 0
@@ -37,5 +36,5 @@ export default config([
       globals: globals.node
     }
   },
-  eslintPluginPrettier
-]);
+  prettierPlugin
+);
